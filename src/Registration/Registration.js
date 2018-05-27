4d05ddbf9c50db61adb4503a54199e74ad8d329a
logeshkumar <logeshkr@thoughtworks.com>
2018-05-27 14:04:48 +0530

    seperated out registration service and added individual error messages

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './Registration.css';
 import { Button, FormControl, Label } from 'react-bootstrap';
-import axios from 'axios';
-import settings from '../Config/configs.js'
+import { postRequest } from './Service'
 class Register extends Component {
   constructor() {
     super();
@@ -11,7 +10,7 @@ class Register extends Component {
       last_name: '',
       email: '',
       company_name: '',
-      error_message: '',
+      error_message: {},
       success_message: ''
     };
 
@@ -25,32 +24,12 @@ class Register extends Component {
 
   onSubmit() {
     this.setState({ error_message: '', success_message: '' });
-    axios.defaults.withCredentials = true;
-    var base_url = '';
-    console.log(process.env);
-    if (process.env.environment)
-      base_url = settings.serice_url[process.env.environment];
-    else
-      base_url = settings.service_url.local;
-    axios.post('http://payroll-service.herokuapp.com/authentication/register/', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      name: this.state.company_name,
-      creator: {
-        email: this.state.email,
-        first_name: this.state.first_name,
-        last_name: this.state.last_name,
-        password: this.textInput.value
-      }
-    })
+      postRequest(this.state, this.textInput.value)
       .then(response => {
-        console.log(response);
-        this.setState({ success_message: 'Thankyou! Verification email has been sent to your registered email' });
-      })
-      .catch(err => {
-        console.log(err);
-        this.setState({ error_message: 'Sorry! Please Enter Valid Data' });
+        if(response.successMessage)
+          this.setState({ success_message: response.successMessage });
+        else
+          this.setState({ error_message: response.errorMessages });
       });
   }
 
@@ -59,20 +38,39 @@ class Register extends Component {
       <div style={{ background: 'mintcream' }}>
         <div className="Register">
           <div>
+            <Label className="error">{this.state.error_message['first_name']}</Label>
+          </div>
+          <div>
             <Label className="label"> First Name </Label>
             <FormControl onChange={this.handleChange} name="first_name" className="form-control" placeholder="Enter First name.."></FormControl>
+          </div>
+
+          <div>
+            <Label className="error">{this.state.error_message['last_name']}</Label>
           </div>
           <div>
             <Label className="label"> Last Name </Label>
             <FormControl onChange={this.handleChange} name="last_name" className="form-control" placeholder="Enter last name.."></FormControl>
           </div>
+
+          <div>
+            <Label className="error">{this.state.error_message['email']}</Label>
+          </div>
           <div>
             <Label className="label"> Email </Label>
             <FormControl onChange={this.handleChange} name="email" className="form-control" placeholder="Enter valid email-id.."></FormControl>
           </div>
+
+          <div>
+            <Label className="error">{this.state.error_message['name']}</Label>
+          </div>
           <div>
             <Label className="label"> Company Name </Label>
             <FormControl onChange={this.handleChange} name="company_name" className="form-control" placeholder="Enter your organisation name.."></FormControl>
+          </div>
+
+          <div>
+            <Label className="error">{this.state.error_message['password']}</Label>
           </div>
           <div>
             <Label className="label"> Password </Label>
@@ -80,9 +78,7 @@ class Register extends Component {
               id="Password" label="Password" type="password" className="form-control" placeholder="Strong password please!" />
           </div>
           <Button onClick={this.onSubmit} className="button" bsStyle="success">Register</Button>
-          <div>
-            <Label className="error">{this.state.error_message}</Label>
-          </div>
+
           <div>
             <Label className="success">{this.state.success_message}</Label>
           </div>
